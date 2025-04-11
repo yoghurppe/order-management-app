1c7495194aac8ed37141f7fa7c955661b17a2898
yoghurppe <yoghurppe3141@gmail.com>
2025-04-11 17:40:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ if mode == "📚 商品情報DB検索":
             df_upload.columns = df_upload.columns.str.strip().str.lower()
             df_upload["jan"] = df_upload["jan"].astype(str).str.strip()
             df_upload = df_upload.drop_duplicates(subset="jan", keep="last")
-            
             for _, row in df_upload.iterrows():
                 requests.post(
                     f"{SUPABASE_URL}/rest/v1/item_master?on_conflict=jan",
