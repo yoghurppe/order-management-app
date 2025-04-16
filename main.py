4c50efc99eb23ebf7cc64a83636d5b1231a08049
yoghurppe <yoghurppe3141@gmail.com>
2025-04-16 14:02:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -157,9 +157,7 @@ if mode == "📦 発注AI判定":
     df_purchase["price"] = pd.to_numeric(df_purchase["price"], errors="coerce").fillna(0)
 
     with st.spinner("🤖 発注AIを計算中..."):
-    results = []
-
-    for _, row in df_sales.iterrows():
+        results = []
 
     for _, row in df_sales.iterrows():
         jan = row["jan"]
@@ -238,6 +236,7 @@ if mode == "📦 発注AI判定":
             )
     else:
         st.info("現在、発注が必要な商品はありません。")
+
 
 
 # 商品情報検索
